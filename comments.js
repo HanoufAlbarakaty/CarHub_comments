be357eefaa1552abf405537c38cc3dae673ee831
HanoufAlbarakaty <han-7879@outlook.com>
2024-08-08 16:27:58 +0300

    Added the isDeleted functionality

--- comments.js
+++ comments.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 
 // Define the schema for the comment
 const commentSchema = new mongoose.Schema({
-  content: { type: String, required: true } // Content is required and should be a string
+  content: { type: String, required: true }, // Content is required and should be a string
+  isDeleted: { type: Boolean, default: false}
 });
+const Comment = mongoose.model('Comment', commentSchema);
 
-// Create and export the Comment model based on the schema
-module.exports = mongoose.model('Comment', commentSchema);
+module.exports = Comment;
